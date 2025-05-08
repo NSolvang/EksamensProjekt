b899bcce630a74daaa54c3d6328b032751e8be27
Nicl841 <eaa24nkjostudents@gmail.com>
2025-05-08 08:43:29 +0200

    Tilføjet validering til brugeroprettelse

--- Core/User.cs
+++ Core/User.cs
@@ -1,16 +1,23 @@
+using System.ComponentModel.DataAnnotations;
 using System;
 namespace Core;
 
 public class User
 {
     
+    
     public int UserId { get; set; }
     
+    [Required(ErrorMessage = "Brugernavn er påkrævet")]
     public string UserName { get; set; }
     
+    [Required(ErrorMessage = "Adgangskode er påkrævet")]
+    [StringLength(10, MinimumLength = 8, ErrorMessage = "Adgangskoden skal være mellem 8 og 10 tegn")]
     public string Password { get; set; }
     
+    [Required(ErrorMessage = "Rolle er påkrævet")]
     public string Role { get; set; }
     
+    [Required(ErrorMessage = "Navn er påkrævet")]
     public string Name { get; set; }
 }
